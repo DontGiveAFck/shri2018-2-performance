eef7534065b0fb65d7d3edbc565580f2964f3a12
Arsen <simf.arsenio@gmail.com>
2018-10-31 04:47:17 +0300

    js optimization

--- scripts2.js
+++ scripts2.js
@@ -48,6 +48,11 @@ const INDICATOR_OFFSET = 265;
 const rotateToValue = function(rotate) {
     return Math.floor((Math.abs(rotate * 360 * 1.73 + INDICATOR_OFFSET) / 53) + MIN_VALUE);
 }
+
+
+/**
+ * @param {Number} rotate Количество оборотов от нейтриального положения.
+ */
 function setRotate(rotate) {
     if (rotate > maxRotate) {
         rotate = maxRotate;
@@ -225,34 +230,4 @@ arrowLeftScens.addEventListener('click', function () {
             behavior: 'smooth'
         });
     }
-});
-
-const selectButton = document.querySelector('.filter__select-button');
-const selectButtonText = document.querySelector('.filter__select-button .button__text');
-const selectOptions = document.querySelectorAll('.filter__select-item');
-const popup = document.querySelector('.filter__select-popup');
-
-selectButton.addEventListener('click', function() {
-    popup.classList.toggle('filter__select-popup_open');
-});
-
-let widths = '';
-window.addEventListener('scroll', function() {
-    widths += document.querySelectorAll('body')[0].offsetWidth;
-    document.querySelector('.stats').innerHTML = widths;
-});
-
-selectOptions.forEach(o => {
-    o.addEventListener('click', function(e) {
-        document.querySelector('#' + e.target.dataset.group).checked = true;
-
-        selectOptions.forEach(opt => opt.classList.toggle('filter__select-item_checked', false));
-        e.target.classList.toggle('filter__select-item_checked', true);
-        popup.classList.toggle('filter__select-popup_open', false);
-        selectButtonText.innerText = e.target.innerText;
-    })
-});
-
-document.querySelector('.menu__icon').addEventListener('click', function () {
-    document.querySelector('.menu').classList.toggle('menu_open');
 });
